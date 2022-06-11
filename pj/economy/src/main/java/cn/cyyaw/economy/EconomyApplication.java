f45caf197ecf6e0b34a14b7792dee34a3e444ba1
why <1663784811@qq.com>
2022-06-12 00:19:55 +0800

    init

---
+++ pj/economy/src/main/java/cn/cyyaw/economy/EconomyApplication.java
@@ -0,0 +1,19 @@
+package cn.cyyaw.economy;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class EconomyApplication {
+
+    public static void main(String[] args) {
+
+
+        SpringApplication.run(EconomyApplication.class, args);
+
+
+    }
+
+}
