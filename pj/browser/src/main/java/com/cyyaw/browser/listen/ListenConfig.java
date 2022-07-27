679b5cdfce47b2fc7cc283fb74548d23ca5298e4
why <1663784811@qq.com>
2022-07-28 00:27:49 +0800

    init

--- pj/browser/src/main/java/com/cyyaw/browser/listen/ListenConfig.java
+++ pj/browser/src/main/java/com/cyyaw/browser/listen/ListenConfig.java
@@ -1,13 +1,56 @@
 package com.cyyaw.browser.listen;
 
 
+import com.cyyaw.browser.controller.spider.SpiderPage;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.event.EventListener;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.stereotype.Component;
+
+import java.util.Map;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 @EnableAsync
+@Component
 public class ListenConfig {
 
+    private static ExecutorService newFixedThreadPool = Executors.newFixedThreadPool(10);
+
+
+    @Autowired
+    private ApplicationContext applicationContext;
+
+
+    public void aaa() {
+
+
+        String url = "";
+
+
+        newFixedThreadPool.submit(new Runnable() {
+            @Override
+            public void run() {
+                spiderFn(url);
+            }
+        });
+    }
+
+    /**
+     * 爬取数据
+     */
+    private void spiderFn(String url) {
+        Map<String, SpiderPage> spiderPageMap = applicationContext.getBeansOfType(SpiderPage.class);
+        for (String key : spiderPageMap.keySet()) {
+            SpiderPage spiderPage = spiderPageMap.get(key);
+            Boolean ok = spiderPage.policyDecision(url);
+            if (ok) {
+                spiderPage.spider(url);
+            }
+        }
+    }
 
 
     /**
@@ -19,21 +62,7 @@ public class ListenConfig {
         // 数据分析
 
 
-        aaa();
-
     }
-
-
-    private void aaa(){
-
-
-
-    }
-
-
-
-
-
 
 
 }
