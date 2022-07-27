3a83f460de570daa2e576895bc02a7c10c7f07cf
why <1663784811@qq.com>
2022-07-27 22:14:37 +0800

    init

--- pj/browser/src/main/java/com/cyyaw/browser/BrowserApplication.java
+++ pj/browser/src/main/java/com/cyyaw/browser/BrowserApplication.java
@@ -1,8 +1,10 @@
 package com.cyyaw.browser;
 
 import com.cyyaw.browser.controller.Hao123Controller;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 
+@SpringBootApplication
 public class BrowserApplication {
 
 
