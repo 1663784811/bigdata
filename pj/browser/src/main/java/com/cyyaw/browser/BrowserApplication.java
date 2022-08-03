599c2ff4a7302ff7016d5d346edb9e501c214d25
why <1663784811@qq.com>
2022-08-04 00:33:08 +0800

    init

--- pj/browser/src/main/java/com/cyyaw/browser/BrowserApplication.java
+++ pj/browser/src/main/java/com/cyyaw/browser/BrowserApplication.java
@@ -2,12 +2,13 @@ package com.cyyaw.browser;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 
 @SpringBootApplication
 public class BrowserApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(BrowserApplication.class, args);
+        ConfigurableApplicationContext run = SpringApplication.run(BrowserApplication.class, args);
     }
 }
--- pj/browser/src/main/java/com/cyyaw/browser/listen/ListenConfig.java
+++ pj/browser/src/main/java/com/cyyaw/browser/listen/ListenConfig.java
@@ -2,6 +2,8 @@ package com.cyyaw.browser.listen;
 
 
 import com.cyyaw.browser.controller.spider.SpiderPage;
+import com.cyyaw.browser.core.Browser;
+import com.cyyaw.browser.core.ChromeBrowser;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.event.EventListener;
@@ -9,6 +11,7 @@ import org.springframework.scheduling.annotation.Async;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.stereotype.Component;
 
+import javax.annotation.PostConstruct;
 import java.util.Map;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
@@ -19,29 +22,37 @@ public class ListenConfig {
 
     private static ExecutorService newFixedThreadPool = Executors.newFixedThreadPool(10);
 
+    Browser browser = new ChromeBrowser();
+
+
+    @PostConstruct
+    public void init(){
+        browser.open("https://www.douyin.com/discover");
+
+        //     第一步：打开浏览器
+
+        //     第二步：打开网址
+
+        //     第三步：收信息
+
+
+
+    }
+
 
     @Autowired
     private ApplicationContext applicationContext;
 
 
     public void aaa() {
-
-
         String url = "";
-
-
-        newFixedThreadPool.submit(new Runnable() {
-            @Override
-            public void run() {
-                spiderFn(url);
-            }
-        });
+        spiderFn(url);
     }
 
     /**
      * 爬取数据
      */
-    private void spiderFn(String url) {
+    public  void spiderFn(String url) {
         Map<String, SpiderPage> spiderPageMap = applicationContext.getBeansOfType(SpiderPage.class);
         for (String key : spiderPageMap.keySet()) {
             SpiderPage spiderPage = spiderPageMap.get(key);
