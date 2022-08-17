3053e5d33ae652eacb91d301eed749e4502d566a
why <1663784811@qq.com>
2022-08-17 17:12:47 +0800

    init

---
+++ demo/nacos/springbootnacos/src/main/java/com/cyyaw/springbootnacos/SpringBootNacosApplication.java
@@ -0,0 +1,21 @@
+package com.cyyaw.springbootnacos;
+
+
+//import com.alibaba.nacos.spring.context.annotation.config.NacosPropertySource;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+//@NacosPropertySource(dataId = "example", autoRefreshed = true)
+public class SpringBootNacosApplication {
+
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(SpringBootNacosApplication.class, args);
+
+
+    }
+
+
+}
---
+++ demo/rabbitmq/mq1/src/main/java/com/cyyaw/mq1/Mq1Application.java
@@ -0,0 +1,19 @@
+package com.cyyaw.mq1;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Mq1Application {
+
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(Mq1Application.class, args);
+
+    }
+
+
+
+}
