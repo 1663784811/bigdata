751db303a96ee664a38afae5dfd3951eb307959b
why <1663784811@qq.com>
2022-09-05 16:41:56 +0800

    init

--- pj/admin/src/main/java/com/cyyaw/admin/table/entity/CpCompany.java
+++ pj/admin/src/main/java/com/cyyaw/admin/table/entity/CpCompany.java
@@ -1,13 +1,16 @@
 package com.cyyaw.admin.table.entity;
 
 import cn.cyyaw.jpa.entity.BaseTable;
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Data;
+import org.springframework.format.annotation.DateTimeFormat;
 
 import javax.persistence.Basic;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Table;
 import java.io.Serializable;
+import java.util.Date;
 
 @Data
 @Entity
@@ -20,6 +23,17 @@ public class CpCompany extends BaseTable implements Serializable {
     @Basic
     @Column(name = "name", columnDefinition = "varchar(255) COMMENT '公司名称'")
     private String name;
+
+    @Basic
+    @Column(name = "establish_time", columnDefinition = "datetime COMMENT '成立时间'")
+    @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    private Date establishTime;
+
+
+    @Basic
+    @Column(name = "legal_person", columnDefinition = "varchar(255) COMMENT '法人'")
+    private String legalPerson;
 
 
 
