267dde8075741042e79916ff3ebae98d79c60ddd
why <1663784811@qq.com>
2022-09-14 01:08:30 +0800

    init

--- pj/browser/src/main/java/com/cyyaw/browser/controller/data10jqka/data/Data10jqkaDataAnalyze.java
+++ pj/browser/src/main/java/com/cyyaw/browser/controller/data10jqka/data/Data10jqkaDataAnalyze.java
@@ -1,8 +1,11 @@
 package com.cyyaw.browser.controller.data10jqka.data;
 
 
+import cn.hutool.core.net.url.UrlBuilder;
 import com.cyyaw.browser.controller.DataAnalyze;
 import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -11,11 +14,31 @@ public class Data10jqkaDataAnalyze  implements DataAnalyze {
 
     @Override
     public boolean urlRule(String url) {
-        return false;
+        UrlBuilder builder = UrlBuilder.ofHttp(url);
+        String host = builder.getHost();
+        if (host.equals("data.10jqka.com.cn")) {
+            return true;
+        } else {
+            return false;
+        }
     }
 
     @Override
-    public void analyze(Document document) {
+    public void analyze(Document doc) {
+        Elements rows = doc.select(".m-table tbody tr");
 
+        for (Element element : rows) {
+            String text = element.text();
+            System.out.println(text);
+
+        }
     }
+
+
+
+
+
+
+
+
 }
--- pj/browser/src/main/java/com/cyyaw/browser/controller/data10jqka/spider/Data10jqkaSpider.java
+++ pj/browser/src/main/java/com/cyyaw/browser/controller/data10jqka/spider/Data10jqkaSpider.java
@@ -1,23 +1,41 @@
 package com.cyyaw.browser.controller.data10jqka.spider;
 
 
+import cn.hutool.core.net.url.UrlBuilder;
 import com.cyyaw.browser.controller.SpiderPageAbstract;
+import org.springframework.context.ApplicationContext;
 import org.springframework.stereotype.Component;
 
 @Component
 public class Data10jqkaSpider extends SpiderPageAbstract {
 
 
+    public Data10jqkaSpider(ApplicationContext context) {
+        super(context);
+    }
+
     @Override
     public Boolean policyDecision(String url) {
         // http://data.10jqka.com.cn/funds/ggzjl/
-
-        return null;
+        UrlBuilder builder = UrlBuilder.ofHttp(url);
+        String host = builder.getHost();
+        if (host.equals("data.10jqka.com.cn")) {
+            return true;
+        } else {
+            return false;
+        }
     }
 
 
+    @Override
+    public void spider(String url) {
 
+        super.spider(url);
+        super.spiderFinish();
 
+        // 判断是否有下一页，有则获取下一页数据
+
+    }
 
 
 }
