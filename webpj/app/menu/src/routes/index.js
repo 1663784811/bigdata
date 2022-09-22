a25f0f854b8d38070eed1181b260b445d42f91bf
why <1663784811@qq.com>
2022-09-22 08:41:20 +0800

    init

---
+++ webpj/app/menu/src/routes/index.js
@@ -0,0 +1,25 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import Home from '@/views/Home.vue'
+
+const routes = [
+    {
+        path: '/',
+        name: 'home',
+        component: Home
+    },
+    {
+        path: '/spider',
+        name: 'spider',
+        component: Home
+    }
+]
+
+const router = createRouter({
+    routes,
+    history: createWebHistory(),
+    scrollBehavior() {
+        return { top: 0 }
+    }
+})
+
+export default router
--- webpj/app/menu/vite.config.js
+++ webpj/app/menu/vite.config.js
@@ -1,14 +1,24 @@
-import { defineConfig } from 'vite'
+import {defineConfig} from 'vite'
+import {resolve} from 'path';
 import vue from '@vitejs/plugin-vue'
 import Components from 'unplugin-vue-components/vite';
-import { VantResolver } from 'unplugin-vue-components/resolvers';
+import {VantResolver} from 'unplugin-vue-components/resolvers';
 
-// https://vitejs.dev/config/
+
 export default defineConfig({
-  plugins: [
-      vue(),
-      Components({
-        resolvers: [VantResolver()],
-      }),
-  ]
+    plugins: [
+        vue(),
+        Components({
+            resolvers: [VantResolver()],
+        }),
+    ],
+    server: {
+        port: 8000,
+        hmr: true
+    },
+    resolve: {
+        alias: {
+            "@": resolve(__dirname, "src")
+        }
+    }
 })
