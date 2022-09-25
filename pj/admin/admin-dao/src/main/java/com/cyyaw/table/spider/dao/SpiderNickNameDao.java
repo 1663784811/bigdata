267463fbafebab1fd9bc816c455ce963af373f35
why <1663784811@qq.com>
2022-09-26 02:43:54 +0800

    init

---
+++ pj/admin/admin-dao/src/main/java/com/cyyaw/table/spider/dao/SpiderNickNameDao.java
@@ -0,0 +1,11 @@
+package com.cyyaw.table.spider.dao;
+
+
+import cn.cyyaw.jpa.BaseDao;
+import com.cyyaw.table.spider.entity.SpiderNickName;
+
+
+public interface SpiderNickNameDao extends BaseDao<SpiderNickName, Integer> {
+
+
+}
---
+++ pj/admin/admin-dao/src/main/java/com/cyyaw/table/spider/entity/SpiderNickName.java
@@ -0,0 +1,38 @@
+package com.cyyaw.table.spider.entity;
+
+
+import cn.cyyaw.jpa.entity.BaseTable;
+import lombok.Data;
+
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+import java.io.Serializable;
+
+@Data
+@Entity
+@Table(name = "spider_nick_name")
+@org.hibernate.annotations.Table(appliesTo = "spider_nick_name", comment = "昵称")
+public class SpiderNickName extends BaseTable implements Serializable {
+
+    private static final long serialVersionUID = 157693283L;
+
+    @Basic
+    @Column(name = "host", columnDefinition = "varchar(255) COMMENT '来源域名'")
+    private String host;
+
+    @Basic
+    @Column(name = "url", columnDefinition = "text COMMENT 'url'")
+    private String url;
+
+    @Basic
+    @Column(name = "nick_name", columnDefinition = "varchar(255) COMMENT '昵称'")
+    private String nickName;
+
+    @Basic
+    @Column(name = "content", columnDefinition = "text COMMENT '内容'")
+    private String content;
+
+
+}
