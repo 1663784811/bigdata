60dcda4fc7630146fbf6193213bfcef39ecdb87e
why <1663784811@qq.com>
2022-09-29 00:01:43 +0800

    init

--- webpj/app/menu/src/views/Home.vue
+++ webpj/app/menu/src/views/Home.vue
@@ -1,40 +1,46 @@
 <template>
-  <div>
-    <van-button type="primary">主要按钮</van-button>
-    <van-dropdown-menu>
-      <van-dropdown-item v-model="value1" :options="option1" />
-      <van-dropdown-item v-model="value2" :options="option2" />
-    </van-dropdown-menu>
+  <div class="home">
+    <van-search v-model="value"
+                background="#4fc08d"
+                placeholder="请输入搜索关键词" />
+
+
+
+    <div class="storeList">
+      <div v-for="(item, index) in storeList"  >
+          {{item.storeName}}
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
   name: "Home.vue",
-  setup() {
-    const value1 = 1;
-    const value2 = 'a' ;
-    const option1 = [
-      { text: '全部商品', value: 0 },
-      { text: '新款商品', value: 1 },
-      { text: '活动商品', value: 2 },
-    ];
-    const option2 = [
-      { text: '默认排序', value: 'a' },
-      { text: '好评排序', value: 'b' },
-      { text: '销量排序', value: 'c' },
-    ];
 
+  data() {
     return {
-      value1,
-      value2,
-      option1,
-      option2,
-    };
+      value:'好评',
+      storeList:[
+        {
+          storeName:'听心一号店'
+        },
+        {
+          storeName:'听心二号店'
+        }
+      ]
+    }
   },
+  setup() {
+
+  },
+  methods: {
+    reload() {
+    }
+  }
 }
 </script>
 
-<style scoped>
+<style lang="scss" scoped>
 
 </style>
