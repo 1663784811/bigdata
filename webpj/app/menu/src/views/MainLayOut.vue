c62d0305fff5a84351dca04dc253567f99ebafba
why <1663784811@qq.com>
2022-10-02 13:35:40 +0800

    init

--- webpj/app/menu/src/views/MainLayOut.vue
+++ webpj/app/menu/src/views/MainLayOut.vue
@@ -2,9 +2,9 @@
   <router-view></router-view>
   <div>
     <van-tabbar v-model="active">
-      <van-tabbar-item icon="wap-home">首页</van-tabbar-item>
-      <van-tabbar-item icon="shopping-cart">订单</van-tabbar-item>
-      <van-tabbar-item icon="manager">我的</van-tabbar-item>
+      <van-tabbar-item replace to="/home" icon="wap-home">首页</van-tabbar-item>
+      <van-tabbar-item replace to="/order" icon="shopping-cart">订单</van-tabbar-item>
+      <van-tabbar-item replace to="/user" icon="manager">我的</van-tabbar-item>
     </van-tabbar>
   </div>
 </template>
@@ -16,7 +16,7 @@ import {ref} from 'vue';
 export default {
   name: "LayOut",
   setup() {
-    const active = ref('home');
+    const active = ref(0);
     return {active};
   },
 }
