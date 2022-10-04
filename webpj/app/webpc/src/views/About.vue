d156cbc9a240c6b6e7d29106c08b94bdefacccd3
why <1663784811@qq.com>
2022-10-04 16:44:57 +0800

    init

--- webpj/app/webpc/src/views/About.vue
+++ webpj/app/webpc/src/views/About.vue
@@ -1,15 +1,5 @@
 <template>
-  <div class="headerImg">
-    <div class="overlay-contact"></div>
-    <div class="container">
-      <div class="row text-center d-flex justify-content-center align-items-center row-height">
-        <div class="col-12">
-          <h1>关于我们</h1>
-          <p>不断创新、追求卓越</p>
-        </div>
-      </div>
-    </div>
-  </div>
+  <HeaderImg />
   <div class="aboutBox">
     <h1>关于我们</h1>
     <div>
@@ -34,50 +24,18 @@
 </template>
 
 <script>
+
+import HeaderImg from './common/HeaderImg.vue'
+
 export default {
-  name: "About"
+  name: "About",
+  components:{
+    HeaderImg
+  }
 }
 </script>
 
 <style lang="scss" scoped>
-.headerImg {
-  position: relative;
-  background-image: url("../assets/about-header.png");
-  background-size: cover;
-  background-repeat: no-repeat;
-  background-attachment: fixed;
-  background-position: center -305px;
-  .overlay-contact {
-    display: block;
-    position: absolute;
-    left: 0;
-    right: 0;
-    bottom: 0;
-    top: 0;
-    background-color: rgba(0, 0, 0, 0.70);
-  }
-  .row-height {
-    height: 50vh;
-    display: flex;
-    align-items: center;
-    .col-12 {
-      -ms-flex: 0 0 100%;
-      flex: 0 0 100%;
-      max-width: 100%;
-      position: relative;
-      width: 100%;
-      padding-right: 15px;
-      padding-left: 15px;
-      text-align: center;
-      color: white;
-      .main-heading {
-        font-size: 46px;
-        font-weight: 500;
-      }
-    }
-  }
-}
-
 .aboutBox {
   text-align: center;
 
---
+++ webpj/app/webpc/src/views/common/HeaderImg.vue
@@ -0,0 +1,63 @@
+<template>
+  <div class="headerImg">
+    <div class="overlay-contact"></div>
+    <div class="container">
+      <div class="row text-center d-flex justify-content-center align-items-center row-height">
+        <div class="col-12">
+          <h1>关于我们</h1>
+          <p>不断创新、追求卓越</p>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HeaderImg"
+}
+</script>
+
+<style lang="scss" scoped>
+.headerImg {
+  position: relative;
+  background-image: url("../../assets/about-header.png");
+  background-size: cover;
+  background-repeat: no-repeat;
+  background-attachment: fixed;
+  background-position: center -305px;
+
+  .overlay-contact {
+    display: block;
+    position: absolute;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    top: 0;
+    background-color: rgba(0, 0, 0, 0.70);
+  }
+
+  .row-height {
+    height: 50vh;
+    display: flex;
+    align-items: center;
+
+    .col-12 {
+      -ms-flex: 0 0 100%;
+      flex: 0 0 100%;
+      max-width: 100%;
+      position: relative;
+      width: 100%;
+      padding-right: 15px;
+      padding-left: 15px;
+      text-align: center;
+      color: white;
+
+      .main-heading {
+        font-size: 46px;
+        font-weight: 500;
+      }
+    }
+  }
+}
+</style>
