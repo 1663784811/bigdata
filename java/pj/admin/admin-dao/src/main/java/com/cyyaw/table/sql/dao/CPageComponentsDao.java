e9440faff6a34c6d6dc88e259236ea5ebed5d71c
why <1663784811@qq.com>
2022-10-31 23:22:22 +0800

    init

---
+++ java/pj/admin/admin-dao/src/main/java/com/cyyaw/table/sql/dao/CPageComponentsDao.java
@@ -0,0 +1,10 @@
+package com.cyyaw.table.sql.dao;
+
+
+import com.cyyaw.jpa.BaseDao;
+import com.cyyaw.table.sql.entity.CPageComponents;
+
+public interface CPageComponentsDao extends BaseDao<CPageComponents, Integer> {
+
+
+}
