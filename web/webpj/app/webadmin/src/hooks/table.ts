aea76861f9c5a5a7c6c5493e9b44bb774a61db06
why <1663784811@qq.com>
2022-11-04 17:29:55 +0800

    init

--- web/webpj/app/webadmin/src/hooks/table.ts
+++ web/webpj/app/webadmin/src/hooks/table.ts
@@ -55,7 +55,7 @@ export const useTableHeight = async function (): Promise<number> {
  */
 
 export const useTable = function <T = any>(tableConfig?: any): Table<T> {
-  const tableColumns = tableConfig ? (tableConfig.column as any) : []
+  const tableColumns = tableConfig && tableConfig.data ? (tableConfig.data as any) : []
   const dataList = ref<Array<T>>()
   const selectRows = ref<Array<string | number>>()
   const tableHeaderRef = ref<TableHeaderType | null>(null)
