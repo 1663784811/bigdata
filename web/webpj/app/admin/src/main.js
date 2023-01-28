1609bb48d401cfebc19fa6aaf30395e7e0c989e4
why <1663784811@qq.com>
2023-01-28 13:17:32 +0800

    init

--- web/webpj/app/admin/src/main.js
+++ web/webpj/app/admin/src/main.js
@@ -1,12 +1,13 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import naive from 'naive-ui'
+import { createPinia } from 'pinia'
+import {router} from "@/router/index";
 
-import {router} from "@/router";
 
-
+const pinia = createPinia()
 const app = createApp(App);
 app.use(router);
-
+app.use(pinia);
 app.use(naive);
 app.mount('#app');
