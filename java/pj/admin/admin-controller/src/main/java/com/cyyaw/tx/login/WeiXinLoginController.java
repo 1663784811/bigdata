482d0e827eea44bfe66be364ba2ba915ee9d7d1c
why <1663784811@qq.com>
2023-02-06 03:02:50 +0800

    init

--- java/pj/admin/admin-controller/src/main/java/com/cyyaw/tx/login/WeiXinLoginController.java
+++ java/pj/admin/admin-controller/src/main/java/com/cyyaw/tx/login/WeiXinLoginController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/login/weixin")
-public class WeixinLoginController {
+public class WeiXinLoginController {
 
 
     public void login(){
