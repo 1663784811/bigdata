a0262b3849976150891c15eb0fc1833f335c2f90
why <1663784811@qq.com>
2023-02-17 01:56:57 +0800

    init

--- web/webpj/app/admin/src/views/layout/LeftMenu.vue
+++ web/webpj/app/admin/src/views/layout/LeftMenu.vue
@@ -1,15 +1,18 @@
 <template>
-  <Menu v-if="store.leftMenu && store.leftMenu.length>0">
-    <Submenu name="1" v-for="(item,index) in store.leftMenu" :key="index">
-      <template #title>
-        <Icon type="ios-paper"/>
-        内容管理
-      </template>
-      <MenuItem name="1-1">文章管理</MenuItem>
-      <MenuItem name="1-2">评论管理</MenuItem>
-      <MenuItem name="1-3">举报管理</MenuItem>
-    </Submenu>
-  </Menu>
+  <div style="width: 200px; background: #aaa">
+    <Menu v-if="store.leftMenu && store.leftMenu.length>0" width="200px">
+      <Submenu name="1" v-for="(item,index) in store.leftMenu" :key="index">
+        <template #title>
+          <Icon type="ios-paper"/>
+          内容管理
+        </template>
+        <MenuItem name="1-1">文章管理</MenuItem>
+        <MenuItem name="1-2">评论管理</MenuItem>
+        <MenuItem name="1-3">举报管理</MenuItem>
+      </Submenu>
+    </Menu>
+  </div>
+
 </template>
 
 <script setup>
