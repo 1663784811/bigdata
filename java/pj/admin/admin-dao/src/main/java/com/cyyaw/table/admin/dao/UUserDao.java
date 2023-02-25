b6146df3a10b86e165682d7dbbeee8c599ee2939
why <1663784811@qq.com>
2023-02-25 16:20:47 +0800

    init

--- java/pj/admin/admin-dao/src/main/java/com/cyyaw/table/admin/dao/UUserDao.java
+++ java/pj/admin/admin-dao/src/main/java/com/cyyaw/table/admin/dao/UUserDao.java
@@ -8,13 +8,10 @@ import java.util.List;
 
 public interface UUserDao extends BaseDao<UUser, Integer> {
 
-
     @Query("select m from UUser m where m.tid in ( select t.touserid from UFriendsUser t where t.userid = ?1 )")
     List<UUser> findByUserid(String userid);
 
-
-    UUser findFirstByTid(String userid);
-
+    @Query("select m from UUser m where m.type =?1")
     List<UUser> findByType(Integer type);
 
     @Query("select m from UUser m where m.tid in ( select t.userid from UGroupUser t where t.groupid = ?1)")
@@ -22,10 +19,10 @@ public interface UUserDao extends BaseDao<UUser, Integer> {
 
     List<UUser> findByAccount(String account);
 
-    @Query("select m from UUser m where m.openid =?1 and m.unionid = ?2")
+    @Query("select m from UUser m where m.openId =?1 and m.unionId = ?2")
     List<UUser> findByOpenIdAndUnionid(String openid, String unionid);
 
-    @Query("select m from UUser m where m.openid =?1")
+    @Query("select m from UUser m where m.openId =?1")
     List<UUser> findByOpenId(String openid);
 
 }
