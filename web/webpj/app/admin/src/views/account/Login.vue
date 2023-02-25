5a866ababa58aae5c7e72534a2b1a368835f0010
why <1663784811@qq.com>
2023-02-25 13:06:32 +0800

    init

--- web/webpj/app/admin/src/views/account/Login.vue
+++ web/webpj/app/admin/src/views/account/Login.vue
@@ -1,33 +1,57 @@
 <template>
   <div class="winHeight">
+    <div class="loginContent">
+      <div class="contentBox">
+        <div class="loginBox">
+          <div>登录</div>
+          <div class="loginRow">
+            <Input  placeholder="用户名" />
+          </div>
+          <div class="loginRow">
+            <Input  placeholder="密码" />
+          </div>
+          <div class="loginRow">
+            <Input  placeholder="用户名" />
+          </div>
+          <div>
+            sd
+          </div>
+        </div>
+      </div>
 
-    <div class="loginBox">
-      <login-box/>
+
     </div>
+    <AccountFooter/>
   </div>
 </template>
 
-<script>
+<script setup>
 
-import {defineComponent} from 'vue'
-import LoginBox from "@/views/account/LoginBox.vue";
-
-export default defineComponent({
-  name: 'Login',
-  components: {
-    'login-box': LoginBox
-  },
-  setup() {
-    return {}
-  },
-})
-</script>
-<style scoped>
-.winHeight{
+import AccountFooter from "./AccountFooter.vue"</script>
+<style scoped lang="less">
+.winHeight {
   height: 100vh;
+
+  .loginContent {
+    padding: 60px 0;
+    background: #c8c8e3;
+
+    .contentBox {
+      display: flex;
+      flex-direction: row-reverse;
+      max-width: 1920px;
+      margin: auto;
+      background: #333;
+      padding:0 60px;
+      .loginBox {
+        width: 400px;
+        border-radius: 6px;
+        background: #fff;
+      }
+    }
+  }
+
 }
-.loginBox {
-  width: 400px;
-}
+
 
 </style>
