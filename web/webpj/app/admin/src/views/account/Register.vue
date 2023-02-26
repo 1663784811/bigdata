91066512d24f396a78aab92485c15a803eae7719
why <1663784811@qq.com>
2023-02-26 12:43:20 +0800

    init

--- web/webpj/app/admin/src/views/account/Register.vue
+++ web/webpj/app/admin/src/views/account/Register.vue
@@ -1,21 +1,36 @@
 <template>
-  <div>
-    <div class="loginBox">
-      <RegisterBox/>
+  <div class="container">
+    <div>注册</div>
+    <div class="steps">
+      <Steps :current="1">
+        <Step title="企业" content="请填写企业基本信息"></Step>
+        <Step title="负责人" content="请填写负责人信息"></Step>
+        <Step title="完成" content="注册完成"></Step>
+      </Steps>
+    </div>
+    <div class="registerBox">
+
+      <div>
+
+
+      </div>
+
+      <div>
+        <Button type="success">上一步</Button>
+        <Button type="success">下一步</Button>
+      </div>
+
     </div>
   </div>
 </template>
 
 <script setup>
 
-import RegisterBox from "@/views/account/RegisterBox.vue";
 
 
 </script>
 <style scoped>
-.loginBox {
-  width: 400px;
-  margin: 20vh auto auto;
+.steps{
+  margin-top: 60px;
 }
-
 </style>
