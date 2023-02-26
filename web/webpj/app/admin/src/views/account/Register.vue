1d7c407a2479e83d10aae94cd683a3741e2de775
why <1663784811@qq.com>
2023-02-26 13:07:06 +0800

    init

--- web/webpj/app/admin/src/views/account/Register.vue
+++ web/webpj/app/admin/src/views/account/Register.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="container">
-    <div>注册</div>
+    <div class="title">企业注册</div>
     <div class="steps">
       <Steps :current="1">
         <Step title="企业" content="请填写企业基本信息"></Step>
@@ -10,16 +10,48 @@
     </div>
     <div class="registerBox">
 
-      <div>
-
-
+      <div class="inputBox">
+        <div class="inputRow">
+          <div class="label">企业名称:</div>
+          <Input prefix="ios-contact" placeholder="企业名称"/>
+        </div>
+        <div class="inputRow">
+          <div class="label">简称:</div>
+          <Input prefix="ios-contact" placeholder=""/>
+        </div>
+        <div class="inputRow">
+          <div class="label">地址:</div>
+          <Input prefix="ios-contact" placeholder="Enter name"/>
+        </div>
+        <div class="inputRow">
+          <div class="label">行业:</div>
+          <Input prefix="ios-contact" placeholder="Enter name"/>
+        </div>
       </div>
 
-      <div>
+      <div class="inputBox">
+        <div class="inputRow">
+          <div class="label">负责人名称:</div>
+          <Input prefix="ios-contact" placeholder="企业名称"/>
+        </div>
+        <div class="inputRow">
+          <div class="label">手机号:</div>
+          <Input prefix="ios-contact" placeholder="Enter name"/>
+        </div>
+        <div class="inputRow">
+          <div class="label">密码:</div>
+          <Input prefix="ios-contact" placeholder=""/>
+        </div>
+        <div class="inputRow">
+          <div class="label">确认密码:</div>
+          <Input prefix="ios-contact" placeholder="Enter name"/>
+        </div>
+      </div>
+
+      <div class="btnBox">
         <Button type="success">上一步</Button>
         <Button type="success">下一步</Button>
       </div>
-
     </div>
   </div>
 </template>
@@ -27,10 +59,48 @@
 <script setup>
 
 
-
 </script>
-<style scoped>
-.steps{
-  margin-top: 60px;
+<style scoped lang="less">
+.title {
+  text-align: center;
+  font-size: 20px;
+  padding: 60px 0 20px 0;
+}
+
+.steps {
+  padding: 60px 0;
+  max-width: 800px;
+  margin: auto;
+}
+
+.registerBox {
+  max-width: 800px;
+  margin: auto;
+
+  .inputBox {
+    .inputRow {
+      display: flex;
+      justify-content: center;
+      justify-items: center;
+      align-items: center;
+      padding: 10px 0;
+
+      .label {
+        width: 120px;
+        font-size: 14px;
+        font-weight: bold;
+        padding-right: 10px;
+        text-align: right;
+      }
+    }
+
+  }
+
+  .btnBox {
+    padding: 20px 0;
+    display: flex;
+    align-items: center;
+    justify-content: center
+  }
 }
 </style>
