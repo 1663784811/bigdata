de23781338ab395dc26478281565c8d9c450a6a5
why <1663784811@qq.com>
2023-03-24 21:40:12 +0800

    init

--- web/webpj/app/admin/src/router/enterprise.js
+++ web/webpj/app/admin/src/router/enterprise.js
@@ -1,6 +1,16 @@
 export const enterprise = {
+    path: '/enterprise',
+    children: [
+        {
+            path: 'List',
+            component: import('@/views/enterprise/List.vue'),
+            name: 'enterprise',
+        },
+        {
+            path: 'storeList',
+            component: import('@/views/enterprise/StoreList.vue'),
+            name: 'storeList',
+        }
+    ]
 
-    path: '/enterprise/List',
-    component: import('@/views/enterprise/List.vue'),
-    name: 'enterprise'
 }
