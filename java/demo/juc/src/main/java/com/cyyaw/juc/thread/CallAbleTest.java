fc283ce7f446da4f7506ad936d386fcdcae31754
why <xxxxxx@xxx.xxx>
2023-05-04 02:55:10 +0800

    init

--- java/demo/juc/src/main/java/com/cyyaw/juc/thread/CallAbleTest.java
+++ java/demo/juc/src/main/java/com/cyyaw/juc/thread/CallAbleTest.java
@@ -1,59 +1,18 @@
 package com.cyyaw.juc.thread;
 
-import java.util.ArrayList;
+import cn.hutool.core.util.RandomUtil;
+
 import java.util.Date;
-import java.util.List;
-import java.util.concurrent.*;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Future;
+import java.util.concurrent.FutureTask;
 
-public class CallAbleTest{
-    //1，2主要区别是创建线程的方式
+public class CallAbleTest {
     public static void main(String[] args) throws ExecutionException, InterruptedException {
-        test1();
-        System.out.println("=========================  结束 ");
-        test2();
-        System.out.println("=========================  主线程结束 ");
-    }
-
-    /**
-     * 使用Executors.newFixedThreadPool创建线程池
-     *
-     * @throws InterruptedException
-     * @throws ExecutionException
-     */
-    private static void test1() throws InterruptedException, ExecutionException {
-        System.out.println("----程序开始运行----");
-        Date date1 = new Date();
-        int taskSize = 5;
-        ExecutorService pool = Executors.newFixedThreadPool(taskSize);
-        List<Future> list = new ArrayList<Future>();
-        for (int i = 0; i < taskSize; i++) {
-            Callable c = new C(i);
-            // 执行任务并获取Future对象
-            Future f = pool.submit(c);
-            list.add(f);
-        }
-        // 关闭线程池
-        pool.shutdown();
-        // 获取所有并发任务的运行结果
-        for (Future f : list) {
-            // 从Future对象上获取任务的返回值，并输出到控制台
-            System.out.println(">>>" + f.get().toString()); //OPTION + return 抛异常
-        }
-        Date date2 = new Date();
-        System.out.println("----程序结束运行----，程序运行时间【" + (date2.getTime() - date1.getTime()) + "毫秒】");
-    }
-
-    /**
-     * 线程直接使用new Thread来创建
-     *
-     * @throws ExecutionException
-     * @throws InterruptedException
-     */
-    private static void test2() throws ExecutionException, InterruptedException {
         System.out.println("----程序开始运行----");
         Date date1 = new Date();
         FutureTask[] randomNumberTasks = new FutureTask[5];
-        List<Future> list = new ArrayList<Future>();
         for (int i = 0; i < randomNumberTasks.length; i++) {
             Callable c = new C(i);
             // 执行任务并获取Future对象
@@ -61,11 +20,15 @@ public class CallAbleTest{
             Thread t = new Thread(randomNumberTasks[i]);
             t.start();
         }
-        System.out.println("ssssssssssssssss");
-        // 获取所有并发任务的运行结果
-        for (Future f : randomNumberTasks) {
-            // 从Future对象上获取任务的返回值，并输
-            System.out.println(">>>" + f.get().toString()); //OPTION + return 抛异常
+        int a = RandomUtil.randomInt(100);
+        if (a % 2 != 0) {
+            System.out.println("------- 主线程  【引用】 子线程 ----------");
+            for (Future f : randomNumberTasks) {// 获取所有并发任务的运行结果
+                System.out.println(">>>" + f.get().toString()); // 从Future对象上获取任务的返回值，并输
+            }
+        } else {
+            System.out.println("------- 主线程没引用子线程 ----------");
+
         }
         Date date2 = new Date();
         System.out.println("----程序结束运行----，程序运行时间【" + (date2.getTime() - date1.getTime()) + "毫秒】");
@@ -86,9 +49,11 @@ class C implements Callable<Object> {
     public Object call() throws Exception {
         System.out.println(">>>" + taskNum + "任务启动");
         Date dateTmp1 = new Date();
-        Thread.sleep(1000);
+        int ins = RandomUtil.randomInt(15) * 1000;
+        Thread.sleep(ins);
         Date dateTmp2 = new Date();
         long time = dateTmp2.getTime() - dateTmp1.getTime();
+        System.out.println("<<<" + taskNum + "任务完成:【" + time + "毫秒】");
         return taskNum + "任务返回运行结果,当前任务时间【" + time + "毫秒】";
     }
 }
