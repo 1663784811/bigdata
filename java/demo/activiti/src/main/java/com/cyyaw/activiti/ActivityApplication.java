5b59f3113aaec84a44cc13fc00fe49475fb27731
why <1663784811@qq.com>
2023-08-30 08:27:19 +0800

    init

--- java/demo/activiti/src/main/java/com/cyyaw/activiti/ActivityApplication.java
+++ java/demo/activiti/src/main/java/com/cyyaw/activiti/ActivityApplication.java
@@ -2,9 +2,17 @@ package com.cyyaw.activiti;
 
 
 import lombok.extern.slf4j.Slf4j;
+import org.activiti.engine.ProcessEngine;
+import org.activiti.engine.ProcessEngineConfiguration;
+import org.activiti.engine.ProcessEngines;
+import org.activiti.spring.ProcessEngineFactoryBean;
+import org.activiti.spring.SpringProcessEngineConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.Bean;
+
+import javax.sql.DataSource;
 
 @Slf4j
 @SpringBootApplication
@@ -13,12 +21,7 @@ public class ActivityApplication {
 
     public static void main(String[] args) {
         ConfigurableApplicationContext run = SpringApplication.run(ActivityApplication.class, args);
-
-
-
-
-
+        log.info("======================     ok      =====================");
     }
-
 
 }
