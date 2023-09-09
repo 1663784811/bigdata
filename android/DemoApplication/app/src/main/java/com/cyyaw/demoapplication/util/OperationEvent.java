de373b0268fa9fefd715f4cd5b3bfde19635a67f
why <1663784811@qq.com>
2023-09-09 12:56:33 +0800

    init

---
+++ android/DemoApplication/app/src/main/java/com/cyyaw/demoapplication/util/OperationEvent.java
@@ -0,0 +1,19 @@
+package com.cyyaw.demoapplication.util;
+
+
+
+/**
+ * 操作事件
+ */
+public class OperationEvent {
+
+
+    /**
+     * 点击Home键
+     */
+    public static void clickHomeKey() {
+
+    }
+
+
+}
