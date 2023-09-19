5a424dc1e04fd2d16951884c55b6073e337efe20
why <1663784811@qq.com>
2023-09-20 00:25:53 +0800

    init

---
+++ java/pj/admin/admin-service-signin/src/main/java/com/cyyaw/signin/table/dao/SiSignInDao.java
@@ -0,0 +1,11 @@
+package com.cyyaw.signin.table.dao;
+
+import com.cyyaw.jpa.BaseDao;
+import com.cyyaw.signin.table.entity.SiSignIn;
+
+
+public interface SiSignInDao extends BaseDao<SiSignIn, Integer> {
+
+
+
+}
---
+++ java/pj/admin/admin-service-signin/src/main/java/com/cyyaw/signin/table/dao/SiSignLogDao.java
@@ -0,0 +1,11 @@
+package com.cyyaw.signin.table.dao;
+
+import com.cyyaw.jpa.BaseDao;
+import com.cyyaw.signin.table.entity.SiSignLog;
+
+
+public interface SiSignLogDao extends BaseDao<SiSignLog, Integer> {
+
+
+
+}
