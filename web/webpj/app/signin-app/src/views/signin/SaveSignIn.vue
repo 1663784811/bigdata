97e00ba976e75cdd3ea5f67db30cd3f05a3db769
why <1663784811@qq.com>
2023-10-13 17:03:24 +0800

    init

--- web/webpj/app/signin-app/src/views/signin/SaveSignIn.vue
+++ web/webpj/app/signin-app/src/views/signin/SaveSignIn.vue
@@ -3,34 +3,34 @@
   <div class="fromBox">
     <div class="row">
       <van-cell-group>
-        <van-field label-align="right" label="标题" placeholder="标题"/>
+        <van-field v-model="state.saveObj.title" label-align="right" label="标题" placeholder="标题"/>
       </van-cell-group>
     </div>
     <div class="row">
       <van-cell-group>
-        <van-field label-align="right" label="简介" placeholder="活动简介"/>
+        <van-field v-model="state.saveObj.title" label-align="right" label="简介" placeholder="活动简介"/>
       </van-cell-group>
     </div>
     <div class="row">
       <van-cell-group>
-        <van-field label-align="right" label="温馨提示" placeholder="温馨提示"/>
+        <van-field v-model="state.saveObj.tips" label-align="right" label="温馨提示" placeholder="温馨提示"/>
       </van-cell-group>
     </div>
     <div class="row">
       <van-cell-group>
-        <van-field label-align="right" label="开始时间" placeholder="开始时间"/>
+        <van-field v-model="state.saveObj.startTime" label-align="right" label="开始时间" placeholder="开始时间"/>
       </van-cell-group>
     </div>
     <div class="row">
       <van-cell-group>
-        <van-field label-align="right" label="结束时间" placeholder="开始时间"/>
+        <van-field v-model="state.saveObj.endTime" label-align="right" label="结束时间" placeholder="开始时间"/>
       </van-cell-group>
     </div>
 
     <div class="row cell">
       <div class="label">填写手机号</div>
       <div class="inputDiv">
-        <van-radio-group shape="square">
+        <van-radio-group shape="square" v-model="state.saveObj.writePhone">
           <van-radio class="vanRadio" name="1">无需填写手机号</van-radio>
           <van-radio class="vanRadio" name="2">建议填写手机号</van-radio>
           <van-radio class="vanRadio" name="3">需填写并验证手机号</van-radio>
@@ -40,7 +40,7 @@
     <div class="row cell">
       <div class="label">指定</div>
       <div class="inputDiv">
-        <van-radio-group shape="square">
+        <van-radio-group shape="square" v-model="state.saveObj.thePeople">
           <van-radio class="vanRadio" name="1">无指定</van-radio>
           <van-radio class="vanRadio" name="2">只可指定人签到</van-radio>
         </van-radio-group>
@@ -74,10 +74,26 @@
   </div>
 
 
+
 </template>
 
 <script setup>
 import sHeader from '@/components/SimpleHeader.vue'
+import {reactive} from "vue";
+
+
+const state = reactive({
+  saveObj: {
+    title: '',
+    introduction: '',
+    tips: '',
+    startTime: '',
+    endTime: '',
+    writePhone:'',
+    thePeople:''
+  }
+})
+
 
 </script>
 
@@ -103,9 +119,11 @@ import sHeader from '@/components/SimpleHeader.vue'
   .row {
     .inputDiv {
       flex: 1;
-      .vanRadio{
+
+      .vanRadio {
         margin: 6px 0;
       }
+
       .qdBox {
         display: flex;
         align-items: center;
