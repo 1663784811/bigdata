14528feb12aec03d5b541cd23a74a7f945bb774c
1663784811@qq.com <11663784811@qq.com>
2023-12-10 12:25:15 +0800

    init

--- java/pj/admin/admin-controller/src/test/java/com/cyyaw/entity/TreeEntityTest.java
+++ java/pj/admin/admin-controller/src/test/java/com/cyyaw/entity/TreeEntityTest.java
@@ -1,35 +1,35 @@
-package com.cyyaw.entity;
-
-
-import cn.hutool.json.JSONObject;
-import cn.hutool.json.JSONUtil;
-
-public class TreeEntityTest {
-
-    public static void main(String[] args) {
-
-
-        TreeEntity treeEntity = new TreeEntity();
-        for (int i = 0; i < 10; i++) {
-            TreeEntity.Node node = new TreeEntity.Node();
-            node.setTid("" + i);
-            node.setName("mm-"+i);
-            treeEntity.add(node);
-            for (int j = 0; j < 3; j++) {
-                TreeEntity.Node node2 = new TreeEntity.Node();
-                node2.setTid( i + "-" + j);
-                node2.setPid(""+i);
-                node2.setName("ss-"+j);
-                treeEntity.add(node2);
-            }
-        }
-
-        JSONObject entries = new JSONObject(treeEntity);
-        String s = JSONUtil.toJsonStr(entries);
-
-
-        System.out.println(s);
-
-    }
-
-}
+//package com.cyyaw.entity;
+//
+//
+//import cn.hutool.json.JSONObject;
+//import cn.hutool.json.JSONUtil;
+//
+//public class TreeEntityTest {
+//
+//    public static void main(String[] args) {
+//
+//
+//        TreeEntity treeEntity = new TreeEntity();
+//        for (int i = 0; i < 10; i++) {
+//            TreeEntity.Node node = new TreeEntity.Node();
+//            node.setTid("" + i);
+//            node.setName("mm-"+i);
+//            treeEntity.add(node);
+//            for (int j = 0; j < 3; j++) {
+//                TreeEntity.Node node2 = new TreeEntity.Node();
+//                node2.setTid( i + "-" + j);
+//                node2.setPid(""+i);
+//                node2.setName("ss-"+j);
+//                treeEntity.add(node2);
+//            }
+//        }
+//
+//        JSONObject entries = new JSONObject(treeEntity);
+//        String s = JSONUtil.toJsonStr(entries);
+//
+//
+//        System.out.println(s);
+//
+//    }
+//
+//}
