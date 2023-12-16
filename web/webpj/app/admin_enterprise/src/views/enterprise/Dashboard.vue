01811f64501fc5e7d7295c70c5b42bb0f8233d1f
1663784811@qq.com <11663784811@qq.com>
2023-12-16 22:03:31 +0800

    init

--- web/webpj/app/admin_enterprise/src/views/enterprise/Dashboard.vue
+++ web/webpj/app/admin_enterprise/src/views/enterprise/Dashboard.vue
@@ -2,14 +2,32 @@
   <div class="appBox">
     <div class="titleBox">我的应用</div>
     <div class="appRowBox">
-      <div class="rowItem">
-        ss
+      <div class="rowItem" v-for="item in 10">
+        <div class="appLogo">
+          <img
+              src="https://img10.360buyimg.com/seckillcms/s280x280_jfs/t1/115175/20/31509/35660/63abdd50F6828629c/5ede778905042c6a.jpg.avif">
+        </div>
+        <div class="appTitle">
+          小黑屋
+        </div>
       </div>
       <div class="rowItem">
-        ddd
+        <div class="appLogo">
+          <img
+              src="https://img10.360buyimg.com/seckillcms/s280x280_jfs/t1/115175/20/31509/35660/63abdd50F6828629c/5ede778905042c6a.jpg.avif">
+        </div>
+        <div class="appTitle">
+          小黑屋
+        </div>
       </div>
       <div class="rowItem">
-        ssss
+        <div class="appLogo">
+          <img
+              src="https://img10.360buyimg.com/seckillcms/s280x280_jfs/t1/115175/20/31509/35660/63abdd50F6828629c/5ede778905042c6a.jpg.avif">
+        </div>
+        <div class="appTitle">
+          小黑屋
+        </div>
       </div>
     </div>
   </div>
@@ -24,7 +42,6 @@
     <div class="logBox">
       <div class="titleBox">日志</div>
       <div class="logContent">
-
         ssss
       </div>
     </div>
@@ -38,6 +55,15 @@
 
 </template>
 <script setup>
+import {onMounted} from "vue";
+import {commonRequest} from '@/api/api.js'
+
+
+onMounted(() => {
+
+
+})
+
 
 </script>
 
@@ -53,13 +79,37 @@
 
   .appRowBox {
     display: flex;
+    flex-wrap: wrap;
     margin-bottom: 20px;
     background: #fff;
+    padding: 10px 10px 0 10px;
 
     .rowItem {
-      width: 200px;
+      width: 160px;
       height: 160px;
-      margin: 0 10px;
+      margin: 0 10px 10px 0;
+      background: #f7f7f7;
+
+      &:hover {
+        background: #d5d5d5;
+        cursor: pointer;
+      }
+
+      .appLogo {
+        width: 120px;
+        height: 120px;
+        margin: 10px auto auto;
+
+        img {
+          max-width: 100%;
+        }
+      }
+
+      .appTitle {
+        text-align: center;
+        overflow: hidden;
+
+      }
     }
   }
 }
