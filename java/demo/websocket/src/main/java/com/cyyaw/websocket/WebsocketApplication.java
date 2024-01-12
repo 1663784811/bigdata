5a2ec1fe8f4edb9cfa731b75ac92463581137bb8
1663784811@qq.com <11663784811@qq.com>
2024-01-13 04:35:14 +0800

    init

---
+++ java/demo/websocket/src/main/java/com/cyyaw/websocket/WebsocketApplication.java
@@ -0,0 +1,25 @@
+package com.cyyaw.websocket;
+
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.core.env.Environment;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.SQLException;
+
+@Slf4j
+@SpringBootApplication
+public class WebsocketApplication {
+
+    public static void main(String[] args) {
+        ConfigurableApplicationContext run = SpringApplication.run(WebsocketApplication.class, args);
+        log.info("------------ 启动成功 ---------");
+        Environment environment = run.getBean(Environment.class);
+        log.info("打开程序：http://127.0.0.1:" + environment.getProperty("local.server.port"));
+    }
+
+}
