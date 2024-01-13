fbc029b95d0af004c9dbee2786288dc990a9f088
why <1663784811@qq.com>
2024-01-13 09:11:57 +0800

    init

---
+++ java/pj/admin/admin-service-food/src/main/java/com/cyyaw/food/FoodApplication.java
@@ -0,0 +1,28 @@
+package com.cyyaw.food;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.core.env.Environment;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+
+@Slf4j
+@SpringBootApplication
+public class FoodApplication {
+
+
+    public static void main(String[] args) throws Exception {
+        ConfigurableApplicationContext run = SpringApplication.run(FoodApplication.class, args);
+        DataSource ds = run.getBean(DataSource.class);
+        Connection connection = ds.getConnection();
+        log.info("{}", connection.getCatalog());
+        log.info("------------    启动成功 ---------");
+        Environment environment = run.getBean(Environment.class);
+        log.info("打开程序：http://127.0.0.1:" + environment.getProperty("local.server.port"));
+    }
+
+}
+
