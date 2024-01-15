acb2751d6cbfd946182c4ea165f26637a89a61e1
1663784811@qq.com <11663784811@qq.com>
2024-01-15 11:58:06 +0800

    init

--- android/IOT/app/src/main/java/com/cyyaw/iot/activity/fragment/HomeFragment.java
+++ android/IOT/app/src/main/java/com/cyyaw/iot/activity/fragment/HomeFragment.java
@@ -1,5 +1,10 @@
 package com.cyyaw.iot.activity.fragment;
 
+import android.graphics.Bitmap;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
 import com.cyyaw.iot.R;
 
 
@@ -15,11 +20,37 @@ public class HomeFragment extends BaseFragment {
 
     @Override
     protected int initLayout() {
-        return R.layout.fr_simple_card;
+        return R.layout.fragment_home;
     }
 
     @Override
     protected void initView() {
+        WebView webView = mRootView.findViewById(R.id.webView);
+        WebSettings settings = webView.getSettings();
+        settings.setJavaScriptEnabled(true);
+        settings.setAllowFileAccess(true);
+        settings.setBuiltInZoomControls(true);
+        webView.loadUrl("http://www.baidu.com");
+        webView.setWebViewClient(new WebViewClient() {
+
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, String url) {
+                view.loadUrl(url);
+                return true;
+            }
+
+            @Override
+            public void onPageStarted(WebView view, String url, Bitmap favicon) {
+                // TODO Auto-generated method stub
+                super.onPageStarted(view, url, favicon);
+            }
+
+            @Override
+            public void onPageFinished(WebView view, String url) {
+                // TODO Aut (view, url);
+            }
+
+        });
 
     }
 
