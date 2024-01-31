b292e51266e6374e58c04609d3017830b781703f
why <1663784811@qq.com>
2024-01-31 22:17:22 +0800

    init

--- android/socialize/app/src/main/cpp/native-lib.cpp
+++ android/socialize/app/src/main/cpp/native-lib.cpp
@@ -1,18 +1,23 @@
 #include <jni.h>
 #include <string>
 
+extern "C" {
+#include <libavcodec/avcodec.h>
+#include <libavcodec/defs.h>
+
+}
+
+
 extern "C" JNIEXPORT jstring
 
 JNICALL
-Java_com_example_socialize_MainActivity_stringFromJNI(JNIEnv *env,jobject) {
+Java_com_example_socialize_MainActivity_stringFromJNI(JNIEnv *env, jobject) {
 
 
     std::string hello = "Hello from C++";
 
+    hello.c_str();
 
-
-
-
-    return env->NewStringUTF(hello.c_str());
+    return env->NewStringUTF(av_version_info());
 }
 
