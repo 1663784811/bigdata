3d42ec5587826c89ceae8578b10f18f078b660e7
why <1663784811@qq.com>
2024-01-31 20:35:21 +0800

    init

---
+++ android/socialize/app/src/main/cpp/native-lib.cpp
@@ -0,0 +1,18 @@
+#include <jni.h>
+#include <string>
+
+extern "C" JNIEXPORT jstring
+
+JNICALL
+Java_com_example_socialize_MainActivity_stringFromJNI(JNIEnv *env,jobject) {
+
+
+    std::string hello = "Hello from C++";
+
+
+
+
+
+    return env->NewStringUTF(hello.c_str());
+}
+
---
+++ android/socialize/app/src/main/java/com/example/socialize/MainActivity.java
@@ -0,0 +1,42 @@
+package com.example.socialize;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.socialize.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+
+    // Used to load the 'socialize' library on application startup.
+    static {
+        System.loadLibrary("socialize");
+    }
+
+    private ActivityMainBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        // Example of a call to a native method
+        TextView tv = binding.sampleText;
+        tv.setText(stringFromJNI());
+    }
+
+    /**
+     * A native method that is implemented by the 'socialize' native library,
+     * which is packaged with this application.
+     */
+    public native String stringFromJNI();
+}
+
+
+
+
+
+
