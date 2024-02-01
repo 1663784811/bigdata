fa3c472015a0ca188eab68a72bd7262307337bec
why <1663784811@qq.com>
2024-02-01 23:21:03 +0800

    init

--- android/socialize/app/src/main/java/com/example/socialize/MainActivity.java
+++ android/socialize/app/src/main/java/com/example/socialize/MainActivity.java
@@ -1,32 +1,43 @@
 package com.example.socialize;
 
 import android.os.Bundle;
+import android.os.Environment;
 import android.view.SurfaceView;
+import android.view.View;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.socialize.databinding.ActivityMainBinding;
 
+import java.io.File;
+
 public class MainActivity extends AppCompatActivity {
 
-    static {
-        System.loadLibrary("socialize");
-    }
 
-    private ActivityMainBinding binding;
+    private SurfaceView surfaceView;
+
+    private WangyiPlayer wangyiPlayer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        surfaceView =  findViewById(R.id.surfaceView);
 
-        SurfaceView surfaceView =  findViewById(R.id.surfaceView);
+
+        wangyiPlayer = new WangyiPlayer();
+        wangyiPlayer.setSurfaceView(surfaceView);
 
 
-        
+    }
 
+
+    public void open(View view){
+        File file = new File(Environment.getExternalStorageDirectory(), "");
+
+        wangyiPlayer.start(file.getAbsolutePath());
     }
 
 
---
+++ android/socialize/app/src/main/java/com/example/socialize/WangyiPlayer.java
@@ -0,0 +1,56 @@
+package com.example.socialize;
+
+import android.view.Surface;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+
+import androidx.annotation.NonNull;
+
+public class WangyiPlayer implements SurfaceHolder.Callback {
+
+    static {
+        System.loadLibrary("socialize");
+    }
+
+    private SurfaceHolder surfaceHolder;
+
+    public void setSurfaceView(SurfaceView surfaceView){
+        if(null!=this.surfaceHolder){
+            this.surfaceHolder.removeCallback(this);
+        }
+        this.surfaceHolder = surfaceView.getHolder();
+        this.surfaceHolder.addCallback(this);
+
+    }
+
+
+    @Override
+    public void surfaceCreated(@NonNull SurfaceHolder holder) {
+
+    }
+
+    @Override
+    public void surfaceChanged(@NonNull SurfaceHolder surfaceHolder, int format, int width, int height) {
+
+        this.surfaceHolder = surfaceHolder;
+
+    }
+
+    @Override
+    public void surfaceDestroyed(@NonNull SurfaceHolder holder) {
+
+    }
+
+    public void start(String path) {
+
+//        native_start(path, surfaceHolder.getSurface());
+
+    }
+
+    public native void native_start(String path, Surface surface);
+
+
+
+
+
+}
