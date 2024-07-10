0f984791141886156d4d36ee7be88085d50f3ed8
why <1663784811@qq.com>
2024-07-10 10:09:52 +0800

    init

--- java/demo/rabbitmq/mq1/src/main/java/com/cyyaw/mq1/RabbitConfig.java
+++ java/demo/rabbitmq/mq1/src/main/java/com/cyyaw/mq1/RabbitConfig.java
@@ -13,77 +13,94 @@ public class RabbitConfig {
 
     // ============================================      交换机
 
-    /**
-     * Direct 交换机
-     * 1. 直连交换机的路由算法非常简单：将消息准送到binding key与该消息的routing key相同的队列
-     */
-    @Bean
-    public DirectExchange directExchange() {
-        DirectExchange exchange = new DirectExchange("test_direct_exchange", true, false);
-        return exchange;
-    }
+//    /**
+//     * Direct 交换机
+//     * 1. 直连交换机的路由算法非常简单：将消息准送到binding key与该消息的routing key相同的队列
+//     */
+//    @Bean
+//    public DirectExchange directExchange() {
+//        DirectExchange exchange = new DirectExchange("test_direct_exchange", true, false);
+//        return exchange;
+//    }
+//
+//    /**
+//     * topic
+//     * 主题交换机
+//     * 发送到主题交换机的消息不能有任意的routing key,必须是由点号分开的一串单词，这些单词可以是任意的，但通常是与消息相关的一些特
+//     * <p>
+//     * *  表示匹配任意一个单词
+//     * #  表示匹配任意一个或多个单词
+//     */
+//    @Bean
+//    public TopicExchange topicExchange() {
+//        return new TopicExchange("test_topic_exchange", true, false);
+//    }
+//
+//    /**
+//     * Fanout扇形交换机
+//     * 是最基本的交换机类型，它所能做的事清非常简单广播消息
+//     */
+//    @Bean
+//    public FanoutExchange fanoutExchange() {
+//        return new FanoutExchange("test_fanout_exchange", true, false);
+//    }
+//
+//    /**
+//     * 根据消息头属性而不是路由键来路由消息。绑定时，可以指定一组键值对，
+//     * 只有当消息的头属性与绑定中指定的键值对匹配时，消息才会被路由到队列。
+//     */
+//    @Bean
+//    public HeadersExchange headersExchange() {
+//        return new HeadersExchange("test_headers_exchange", true, false);
+//    }
 
-    /**
-     * topic
-     * 主题交换机
-     * 发送到主题交换机的消息不能有任意的routing key,必须是由点号分开的一串单词，这些单词可以是任意的，但通常是与消息相关的一些特
-     * <p>
-     * *  表示匹配任意一个单词
-     * #  表示匹配任意一个或多个单词
-     */
-    @Bean
-    public TopicExchange topicExchange() {
-        return new TopicExchange("test_topic_exchange", true, false);
-    }
 
-    /**
-     * Fanout扇形交换机
-     * 是最基本的交换机类型，它所能做的事清非常简单广播消息
-     */
     @Bean
-    public FanoutExchange fanoutExchange() {
-        return new FanoutExchange("test_fanout_exchange", true, false);
+    public Exchange eventExchange() {
+        return ExchangeBuilder.topicExchange("amq.rabbitmq.event").durable(true).internal().build();
     }
-
-    /**
-     * 根据消息头属性而不是路由键来路由消息。绑定时，可以指定一组键值对，
-     * 只有当消息的头属性与绑定中指定的键值对匹配时，消息才会被路由到队列。
-     */
-    @Bean
-    public HeadersExchange headersExchange() {
-        return new HeadersExchange("test_headers_exchange", true, false);
-    }
-
 
     // =========================================================================      队列
 
-    /**
-     *
-     */
-    @Bean
-    public Queue topicQueue1() {
-        return new Queue("test_topicQueue1");
-    }
+//    /**
+//     *
+//     */
+//    @Bean
+//    public Queue topicQueue1() {
+//        return new Queue("test_topicQueue1");
+//    }
+//
+//    /**
+//     *
+//     */
+//    @Bean
+//    public Queue topicQueue2() {
+//        return new Queue("test_topicQueue2");
+//    }
+
 
     /**
-     *
+     * 事件队列
      */
     @Bean
-    public Queue topicQueue2() {
-        return new Queue("test_topicQueue2");
+    public Queue eventQueue() {
+        return QueueBuilder.durable( "event_queue").build();
     }
-
 
     // ============================================      队列 绑定 交换机
-    @Bean
-    public Binding topicBinding1() {
-        return BindingBuilder.bind(topicQueue1()).to(topicExchange()).with("ss007.id.*");
-    }
+//    @Bean
+//    public Binding topicBinding1() {
+//        return BindingBuilder.bind(topicQueue1()).to(topicExchange()).with("ss007.id.*");
+//    }
+//
+//    @Bean
+//    public Binding topicBinding2() {
+//        return BindingBuilder.bind(topicQueue2()).to(topicExchange()).with("ss007.name.*");
+//    }
 
     @Bean
-    public Binding topicBinding2() {
-        return BindingBuilder.bind(topicQueue2()).to(topicExchange()).with("ss007.name.*");
+    public Binding binding() {
+        return BindingBuilder.bind(eventQueue()).to(eventExchange()).with("connection.#").noargs();
     }
-
 
 }
--- java/demo/rabbitmq/mq1/src/main/java/com/cyyaw/mq1/RabbitMQService.java
+++ java/demo/rabbitmq/mq1/src/main/java/com/cyyaw/mq1/RabbitMQService.java
@@ -1,10 +1,14 @@
 package com.cyyaw.mq1;
 
 import org.springframework.amqp.core.AmqpTemplate;
+import org.springframework.amqp.core.Message;
+import org.springframework.amqp.core.MessageProperties;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.Map;
 
 @Service
 public class RabbitMQService {
@@ -24,9 +28,26 @@ public class RabbitMQService {
     }
 
 
-    @RabbitListener(queues = "mqtt_test_service")
-    public void listenSimpleQueueMessage(String msg) throws InterruptedException {
-        System.out.println("spring 消费者接收到消息 ：【" + msg + "】");
+    //    @RabbitListener(queues = "mqtt_test_service")
+//    public void listenSimpleQueueMessage(String msg) throws InterruptedException {
+//        System.out.println("spring 消费者接收到消息 ：【" + msg + "】");
+//    }
+//
+//
+    @RabbitListener(queues = "event_queue")
+    public void handleDeviceConnectedEvent(Message message) {
+        MessageProperties messageProperties = message.getMessageProperties();
+        Map<String, Object> headers = messageProperties.getHeaders();
+        String receivedRoutingKey = messageProperties.getReceivedRoutingKey();
+        if ("connection.created".equals(receivedRoutingKey)) {
+            messageProperties.getHeader("queue");
+
+        } else if ("connection.closed".equals(receivedRoutingKey)) {
+            messageProperties.getHeader("queue");
+
+        }
+        System.out.println("" + headers.get("client_properties"));
+        System.out.println("消费者接收到消息:" + receivedRoutingKey + "   " + headers);
     }
 
 }
