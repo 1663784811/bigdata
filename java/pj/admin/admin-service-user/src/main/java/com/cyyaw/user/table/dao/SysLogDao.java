30492851c736b12fe92d6b79b7e881262ef7cd71
why <1663784811@qq.com>
2024-07-19 04:18:58 +0800

    init

---
+++ java/pj/admin/admin-service-user/src/main/java/com/cyyaw/user/table/dao/SysLogDao.java
@@ -0,0 +1,10 @@
+package com.cyyaw.user.table.dao;
+
+
+import com.cyyaw.jpa.BaseDao;
+import com.cyyaw.user.table.entity.SysLog;
+
+public interface SysLogDao extends BaseDao<SysLog, Integer> {
+
+
+}
---
+++ java/pj/admin/admin-service-user/src/main/java/com/cyyaw/user/table/entity/SysLog.java
@@ -0,0 +1,50 @@
+package com.cyyaw.user.table.entity;
+
+import com.alibaba.fastjson.annotation.JSONField;
+import com.cyyaw.jpa.util.entity.BaseEntity;
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.Data;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.util.Date;
+
+@Data
+@Entity
+@Table(name = "sys_log")
+@org.hibernate.annotations.Table(appliesTo = "sys_log", comment = "系统日志")
+public class SysLog implements BaseEntity<Integer>, Serializable {
+
+    private static final long serialVersionUID = 158730117368298L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Basic
+    @Column(name = "id", columnDefinition = "int auto_increment not null COMMENT 'id'")
+    private Integer id;
+    @Basic
+    @Column(name = "tid", columnDefinition = "varchar(32) unique not null default '' COMMENT 'tid'")
+    private String tid;
+    @Basic
+    @Column(name = "create_time", columnDefinition = "datetime default now() COMMENT '创建时间'")
+    @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    private Date createTime;
+    @Basic
+    @Column(name = "del", columnDefinition = "int default '0' COMMENT '是否删除{0:否,1:是}'")
+    private Integer del;
+    @Basic
+    @Column(name = "note", columnDefinition = "varchar(255) default '' COMMENT '备注'")
+    private String note;
+
+    // =================================================================================
+    @Basic
+    @Column(name = "enterprise_code", columnDefinition = "varchar(32) COMMENT '所属企业e_enterprise表code'")
+    private String enterpriseCode;
+
+    // =================================================================================
+    @Basic
+    @Column(name = "log", columnDefinition = "text COMMENT '日志'")
+    private String log;
+}
