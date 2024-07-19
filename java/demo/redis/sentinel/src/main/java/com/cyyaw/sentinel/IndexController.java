148f1b92978b7fff577f313e26a3ff3ff6c4f5d2
why <1663784811@qq.com>
2024-07-19 15:43:20 +0800

    init

--- java/demo/redis/sentinel/src/main/java/com/cyyaw/sentinel/IndexController.java
+++ java/demo/redis/sentinel/src/main/java/com/cyyaw/sentinel/IndexController.java
@@ -3,25 +3,25 @@ package com.cyyaw.sentinel;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
-@RestController
+@Controller
 public class IndexController {
 
 
     @Autowired
-    private RedisTemplate<String,String> redisTemplate;
+    private RedisTemplate<String, String> masterRedisTemplate;
 
-    @RequestMapping(value = {"","index"})
-    public String index(String key){
-        return  redisTemplate.opsForValue().get(key);
-    }
-
-    @RequestMapping(value = {"set"})
-    public String set(String key, String val){
-        redisTemplate.opsForValue().set(key,val);
+    @GetMapping("")
+    @ResponseBody
+    public String index() {
+        masterRedisTemplate.opsForValue().set("key", "木木木木ssssssssssssssssssssssssssssssssssssssssssssss");
+        Object o = masterRedisTemplate.opsForValue().get("key");
+        System.out.println(o);
         return "ok";
     }
+
 
 }
--- java/demo/redis/sentinel/src/main/java/com/cyyaw/sentinel/SentinelApplication.java
+++ java/demo/redis/sentinel/src/main/java/com/cyyaw/sentinel/SentinelApplication.java
@@ -7,13 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SentinelApplication {
 
-
-
-
     public static void main(String[] args) {
         SpringApplication.run(SentinelApplication.class, args);
-
     }
-
-
 }
