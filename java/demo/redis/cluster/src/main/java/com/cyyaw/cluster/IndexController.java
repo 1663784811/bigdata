910e5f7ee8b4b9b6f4b0e922a18da5a358799faf
why <1663784811@qq.com>
2024-07-19 12:46:22 +0800

    init

---
+++ java/demo/redis/cluster/src/main/java/com/cyyaw/cluster/IndexController.java
@@ -0,0 +1,27 @@
+package com.cyyaw.cluster;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class IndexController {
+
+
+    @Autowired
+    private RedisTemplate<String,String> redisTemplate;
+
+    @RequestMapping(value = {"","index"})
+    public String index(String key){
+        return  redisTemplate.opsForValue().get(key);
+    }
+
+    @RequestMapping(value = {"set"})
+    public String set(String key, String val){
+        redisTemplate.opsForValue().set(key,val);
+        return "ok";
+    }
+
+}
