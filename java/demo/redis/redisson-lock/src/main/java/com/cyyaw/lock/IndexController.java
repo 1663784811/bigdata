d1374221d383a64c0dda6c2928f51f4b5f944993
why <1663784811@qq.com>
2024-07-19 17:55:15 +0800

    init

--- java/demo/redis/redisson-lock/src/main/java/com/cyyaw/lock/IndexController.java
+++ java/demo/redis/redisson-lock/src/main/java/com/cyyaw/lock/IndexController.java
@@ -1,15 +1,14 @@
 package com.cyyaw.lock;
 
-
-//import org.redisson.api.RLock;
-//import org.redisson.api.RedissonClient;
+import org.redisson.api.RFuture;
+import org.redisson.api.RLock;
+import org.redisson.api.RedissonClient;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.concurrent.ExecutionException;
 import java.util.concurrent.TimeUnit;
 
 @RestController
@@ -18,8 +17,8 @@ public class IndexController {
     @Autowired
     private RedisTemplate<String, String> masterRedisTemplate;
 
-//    @Autowired
-//    private RedissonClient redissonClient;
+    @Autowired
+    private RedissonClient redissonClient;
 
 
     @GetMapping("")
@@ -27,18 +26,20 @@ public class IndexController {
         masterRedisTemplate.opsForValue().set("key", "木木木木ssssssssssssssssssssssssssssssssssssssssssssss");
         Object o = masterRedisTemplate.opsForValue().get("key");
         System.out.println(o);
+        RLock lock = null;
+        try {
+            lock = redissonClient.getLock("aaa");
+            lock.lock(5, TimeUnit.SECONDS);
+            System.out.println("加锁成功");
+            Thread.sleep(1000 * 5);
+        } catch (Exception e) {
 
-//        RLock lock = redissonClient.getLock("");
-//        try {
-//            boolean isLocked = lock.tryLock(1, TimeUnit.MILLISECONDS);
-//            if (isLocked) {
-//                // TODO
-//            }
-//        } catch (Exception e) {
-//            lock.unlock();
-//        }
-
-
+        } finally {
+            if (null != lock && lock.isLocked()) {
+                lock.unlock();
+                System.out.println("解锁成功");
+            }
+        }
         return "ok";
     }
 
