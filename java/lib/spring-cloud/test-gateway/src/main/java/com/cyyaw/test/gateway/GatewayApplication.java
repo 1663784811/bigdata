896f6e9c49ccc2a842d17a05691763c3b4526780
why <1663784811@qq.com>
2024-07-22 08:00:46 +0800

    init

---
+++ java/lib/spring-cloud/test-gateway/src/main/java/com/cyyaw/test/gateway/GatewayApplication.java
@@ -0,0 +1,51 @@
+package com.cyyaw.test.gateway;
+
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.env.Environment;
+
+@Slf4j
+@EnableDiscoveryClient
+@SpringBootApplication
+public class GatewayApplication {
+
+    public static void main(String[] args) {
+        ConfigurableApplicationContext run = SpringApplication.run(GatewayApplication.class, args);
+        Environment environment = run.getBean(Environment.class);
+        log.info("网关地址：http://127.0.0.1:" + environment.getProperty("local.server.port") + "");
+    }
+
+
+
+//    @Bean
+//    public RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
+//        return builder.routes()
+//                .route("path_route", r -> r.path("/get")
+//                        .uri("http://httpbin.org"))
+//                .route("host_route", r -> r.host("*.myhost.org")
+//                        .uri("http://httpbin.org"))
+//                .route("rewrite_route", r -> r.host("*.rewrite.org")
+//                        .filters(f -> f.rewritePath("/foo/(?<segment>.*)", "/${segment}"))
+//                        .uri("http://httpbin.org"))
+//                .route("hystrix_route", r -> r.host("*.hystrix.org")
+//                        .filters(f -> f.hystrix(c -> c.setName("slowcmd")))
+//                        .uri("http://httpbin.org"))
+//                .route("hystrix_fallback_route", r -> r.host("*.hystrixfallback.org")
+//                        .filters(f -> f.hystrix(c -> c.setName("slowcmd").setFallbackUri("forward:/hystrixfallback")))
+//                        .uri("http://httpbin.org"))
+//                .route("limit_route", r -> r
+//                        .host("*.limited.org").and().path("/anything/**")
+//                        .filters(f -> f.requestRateLimiter(c -> c.setRateLimiter(redisRateLimiter())))
+//                        .uri("http://httpbin.org"))
+//                .build();
+//    }
+
+
+}
