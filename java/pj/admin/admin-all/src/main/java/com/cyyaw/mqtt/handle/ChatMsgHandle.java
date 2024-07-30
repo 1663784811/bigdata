27d88d7e1b055e2afe908b4a3e729113c88a5f7b
why <1663784811@qq.com>
2024-07-30 15:12:15 +0800

    init

--- java/pj/admin/admin-all/src/main/java/com/cyyaw/mqtt/handle/ChatMsgHandle.java
+++ java/pj/admin/admin-all/src/main/java/com/cyyaw/mqtt/handle/ChatMsgHandle.java
@@ -5,18 +5,14 @@ import cn.hutool.json.JSONObject;
 import com.cyyaw.mqtt.MqttService;
 import com.cyyaw.mqtt.MsgData;
 import com.cyyaw.mqtt.MsgType;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
-
-import java.util.HashMap;
-import java.util.Map;
-import java.util.UUID;
-import java.util.concurrent.ConcurrentHashMap;
-import java.util.concurrent.CopyOnWriteArrayList;
 
 /**
  * 聊天消息
  */
+@Slf4j
 @Component
 public class ChatMsgHandle implements MsgHandle {
 
@@ -30,9 +26,15 @@ public class ChatMsgHandle implements MsgHandle {
 
     @Override
     public void handle(String from, String to, String data) {
-        JSONObject entries = new JSONObject(data);
 
+        MsgData msgData = new MsgData();
+        msgData.setType(MsgType.CHAT.getCode());
+        msgData.setData(data);
+        msgData.setFrom(from);
+        msgData.setTo(to);
 
+        mqttService.send(to, new JSONObject(msgData).toString());
     }
+
 
 }
