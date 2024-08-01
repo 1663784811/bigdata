38d1eb441ee6c367c886f5b68bab65b8729dac49
why <1663784811@qq.com>
2024-08-01 18:42:24 +0800

    init

--- web/webpj/app/admin_enterprise/vite.config.js
+++ web/webpj/app/admin_enterprise/vite.config.js
@@ -2,6 +2,12 @@ import {defineConfig} from 'vite'
 import vue from '@vitejs/plugin-vue'
 import {VantResolver} from 'unplugin-vue-components/resolvers';
 import Components from 'unplugin-vue-components/vite';
+// 代码块分析工具
+import {visualizer} from 'rollup-plugin-visualizer'
+// cdn 引入
+import {autoComplete, Plugin as importToCDN} from "vite-plugin-cdn-import"
+// gzip 压缩
+import viteCompression from 'vite-plugin-compression'
 
 import path from 'path';
 
@@ -9,33 +15,46 @@ const srcPath = path.resolve(__dirname, 'src');
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  envDir:"env",
-  base: '/enterprise',
-  resolve: {
-    // 配置路径别名
-    alias: [
-      {find: '@', replacement: srcPath},
+    envDir: "env",
+    base: '/enterprise',
+    resolve: {
+        // 配置路径别名
+        alias: [
+            {find: '@', replacement: srcPath},
+        ],
+    },
+    plugins: [
+        vue(),
+        Components({
+            resolvers: [VantResolver()],
+        }),
+        visualizer({
+            emitFile: false,
+            filename: 'enterprise/stats.html',
+            open: true,
+            sourcemap: true
+        }),
+        viteCompression({
+            verbose: true,
+            disable: false,
+            threshold: 1024,
+            algorithm: 'gzip',
+            deleteOriginFile: false
+        })
     ],
-  },
-  plugins: [
-    vue(),
-    Components({
-      resolvers: [VantResolver()],
-    }),
-  ],
-  server: {
-    host: "0.0.0.0"
-  },
-  build: {
-    // 打包输出的文件夹名称
-    outDir: 'enterprise',
-    // 静态资源文件保存的文件夹名称
-    assetsDir: 'static',
-    // 是否启用css代码拆分
-    cssCodeSplit: true,
-    // 打包构建后是否生成 source map 文件。
-    sourcemap: false,
-    // 打包构建时压缩混淆使用的混淆器
-    minify: 'esbuild',
-  }
+    server: {
+        host: "0.0.0.0"
+    },
+    build: {
+        // 打包输出的文件夹名称
+        outDir: 'enterprise',
+        // 静态资源文件保存的文件夹名称
+        assetsDir: 'static',
+        // 是否启用css代码拆分
+        cssCodeSplit: true,
+        // 打包构建后是否生成 source map 文件。
+        sourcemap: true,
+        // 打包构建时压缩混淆使用的混淆器
+        minify: 'esbuild',
+    }
 })
