3a0b7f794cc47b135b8f777f3de12ce1d1785bbd
why <1663784811@qq.com>
2024-08-04 07:44:04 +0800

    init

--- web/webpj/app/admin_enterprise/src/component/comfig/configPage/ConfigPageForm.vue
+++ web/webpj/app/admin_enterprise/src/component/comfig/configPage/ConfigPageForm.vue
@@ -7,25 +7,32 @@
   <Tabs v-model="configModule.configPage.tabsName" @onClick="(name)=>{configModule.configPage.tabsName = name}">
     <TabPane label="查询" name="查询">
       <div>
-        <ObjRequest :setting="state.queryObj" title="查询地址"/>
+        <ObjRequest :setting="state.outObj.queryObj" title="查询地址"/>
       </div>
     </TabPane>
     <TabPane label="添加" name="添加">
       <div class="configBox">
-        <ObjSave :setting="state.addObj"/>
+        <ObjSave :setting="state.outObj.addObj"/>
       </div>
     </TabPane>
     <TabPane label="更新" name="更新">
       <div class="configBox">
-        <ObjSave :setting="state.updateObj"/>
+        <ObjSave :setting="state.outObj.updateObj"/>
       </div>
     </TabPane>
     <TabPane label="删除" name="删除">
       <div>
-        <ObjRequest :setting="state.delObj" title="删除地址"/>
+        <ObjRequest :setting="state.outObj.delObj" title="删除地址"/>
       </div>
     </TabPane>
   </Tabs>
+
+  <Modal v-model="state.jsonData.show" :loading="state.jsonData.loading" title="数据" width="80vw" @on-ok="saveComponentsFn">
+    <Input v-model="state.jsonData.data" type="textarea" :rows="40"/>
+  </Modal>
+  <Modal v-model="state.showCode.show" title="查看代码" width="80vw" @on-ok="showCodeHandleFn">
+    <Input v-model="state.showCode.data" type="textarea" :rows="30"/>
+  </Modal>
   <DatabaseLoad v-model="state.databaseLoad" @event="loadDataHandleFn"/>
 
 </template>
@@ -35,11 +42,10 @@ import ObjSave from './com/ObjSave.vue'
 import ObjRequest from './com/ObjRequest.vue'
 
 
-import {reactive, onMounted, watch, ref, provide} from 'vue'
-import {Input, Message} from "view-ui-plus";
-import {saveComponents, loadTable} from '@/api/api.js'
+import {onMounted, reactive, watch} from 'vue'
+import {Message} from "view-ui-plus";
+import {saveComponents} from '@/api/api.js'
 import {useConfigModule} from "@/store/configModule.js";
-import draggable from "vuedraggable";
 import {useWinModal} from '@/store/winModal.js'
 
 
@@ -56,35 +62,29 @@ const props = defineProps({
 });
 
 const state = reactive({
-  // ===============  查询
-  queryObj: {
-    url: "/appAdmin/{appid}/common/del",
-    parameter: {
-      code: ""
-    }
+  outObj: {
+    // ===============  查询
+    queryObj: {},
+    // ===============  添加
+    addObj: {},
+    // ===============  更新
+    updateObj: {},
+    // ===============  删除
+    delObj: {},
   },
-  // ===============  添加
-  addObj: {
-    url: "/appAdmin/{appid}/common/del",
-    parameter: {
-      code: ""
-    }
+  databaseLoad: false,
+  showCode: {
+    show: false,
+    data: '',
+    modal: ''
   },
-  // ===============  更新
-  updateObj: {
-    url: "/appAdmin/{appid}/common/del",
-    parameter: {
-      code: ""
-    }
+  jsonData: {
+    show: false,
+    loading: false,
+    id: '',
+    tid: '',
+    data: ''
   },
-  // ===============  删除
-  delObj: {
-    url: "/appAdmin/{appid}/common/del",
-    parameter: {
-      code: ""
-    }
-  },
-  // ===============
 })
 
 
@@ -103,7 +103,7 @@ const loadDefaultFn = (str) => {
  * 显示代码
  */
 const showCodeFn = () => {
-  // state.jsonData.show = true;
+  state.jsonData.show = true;
   compileCode();
 }
 
@@ -112,33 +112,49 @@ const showCodeFn = () => {
  */
 const saveComponentsFn = () => {
   compileCode();
-  // saveComponents({
-  //   id: state.jsonData.id,
-  //   data: state.jsonData.data
-  // }).then(rest => {
-  //   Message.success({
-  //     background: true,
-  //     content: `${rest.msg}`
-  //   });
-  // }).finally(() => {
-  //   state.jsonData.loading = false;
-  // })
+  saveComponents({
+    id: state.jsonData.id,
+    data: state.jsonData.data
+  }).then(rest => {
+    Message.success({
+      background: true,
+      content: `${rest.msg}`
+    });
+  }).finally(() => {
+    state.jsonData.loading = false;
+  })
 }
 
 const compileCode = () => {
-  // state.tableObj.operation = state.operationObj;
-  // const json = {
-  //   searchObj: state.searchObj,
-  //   tableObj: state.tableObj,
-  //   saveObj: state.saveObj,
-  // }
-  // state.jsonData.data = JSON.stringify(json, null, "  ");
+  state.tableObj.operation = state.operationObj;
+  const json = {
+    searchObj: state.state,
+    tableObj: state.tableObj,
+    saveObj: state.saveObj,
+  }
+  state.jsonData.data = JSON.stringify(json, null, "  ");
 }
 
+const loadDataHandleFn = (data) => {
+  state.queryObj = data.queryObj;
+  state.addObj = data.addObj
+  state.updateObj = data.updateObj;
+  state.delObj = data.delObj
+}
+
+const showCodeHandleFn = () => {
+  if (state.showCode.modal === 'table') {
+    state.tableObj = JSON.parse(state.showCode.data);
+  } else if (state.showCode.modal === 'search') {
+    state.searchObj = JSON.parse(state.showCode.data);
+  } else if (state.showCode.modal === 'save') {
+    state.saveObj = JSON.parse(state.showCode.data);
+  }
+}
 
 const initFn = () => {
-  // const {setting} = props;
-  // const {saveObj, tableObj, searchObj, id, tid} = setting
+  const {setting} = props;
+  const {saveObj, tableObj, searchObj, id, tid} = setting
   // if (searchObj) {
   //   state.searchObj = searchObj;
   // }
@@ -157,64 +173,12 @@ const initFn = () => {
   // }
 }
 
-const loadDataHandleFn = (data) => {
-  state.queryObj = data.queryObj;
-  state.addObj = data.addObj
-  state.updateObj = data.updateObj;
-  state.delObj = data.delObj
-}
-
 
 watch(() => props.setting, () => {
   initFn()
-}, {deep: false, immediate: false})
+})
 
 </script>
 <style scoped lang="less">
-.configBox {
-  .headerBox {
-    display: flex;
-    justify-content: space-between;
-    margin-bottom: 10px;
-  }
-
-  .dataContent {
-    min-height: 800px;
-
-    .row {
-      display: flex;
-      align-items: center;
-      padding: 12px 0;
-      border-top: 1px solid #f1f1f1;
-
-      &:hover {
-        background: #f1f1f1;
-      }
-
-      .labelLeft {
-        width: 120px;
-        text-align: right;
-        margin-right: 10px;
-      }
-
-      .rightInput {
-        flex: 1;
-        margin-right: 10px;
-      }
-
-      .rowItem {
-        margin: 0 4px;
-      }
-
-      .sortBtn {
-      }
-
-      .saveTitle {
-        width: 120px;
-        text-align: right;
-      }
-    }
-  }
-}
 
 </style>
