ff299dacbf8b68cab0015d906f7d85aa7954958c
why <1663784811@qq.com>
2024-08-09 13:53:06 +0800

    init

--- web/webpj/app/admin_enterprise/src/component/comfig/configPage/com/ObjRequest.vue
+++ web/webpj/app/admin_enterprise/src/component/comfig/configPage/com/ObjRequest.vue
@@ -6,14 +6,23 @@
       <Input v-model="setting.url" placeholder="url" clearable/>
     </div>
     <div>
-      参数：
-
+      <div>
+        参数：
+        <Button type="success" icon="md-add-circle" @click="state.parameterArr.push({key:'', val:''})" size="small"/>
+      </div>
+      <div v-for="(item, index) in state.parameterArr" :key="index">
+        <Input style="width: 200px" v-model="item.key" @on-change="changeArr" placeholder="key"/>:
+        <Input style="width: 200px" v-model="item.val" @on-change="changeArr" placeholder="val"/>
+        <Button type="error" icon="md-trash" @click="state.parameterArr.splice(index, 1)" size="small"/>
+      </div>
     </div>
 
   </div>
 </template>
 
 <script setup>
+
+import {reactive, watch} from "vue";
 
 const props = defineProps({
   title: {
@@ -33,6 +42,41 @@ const props = defineProps({
     required: false
   }
 });
+
+
+const state = reactive({
+  parameterArr: [],
+  init: false
+})
+
+
+const changeArr = () => {
+  const arr = state.parameterArr;
+  let obj = {}
+  for (let i = 0; i < arr.length; i++) {
+    const arrObj = arr[i];
+    if(arrObj.key){
+      obj[arrObj.key] = arrObj.val;
+    }
+  }
+  props.setting.parameter = obj;
+}
+
+watch(() => state.parameterArr, () => {
+  changeArr()
+})
+watch(() => props.setting.parameter, () => {
+  const {parameter} = props.setting;
+  if (parameter && !state.init) {
+    state.init = true;
+    state.parameterArr = [];
+    for (let key in parameter) {
+      const val = parameter[key];
+      state.parameterArr.push({key: key, val: val})
+    }
+  }
+})
+
 </script>
 
 <style scoped lang="less">
